69334fa22c6c4dfefceaecfca02b2d1e912b0685
Tangerie <josh.cavill01@gmail.com>
2023-04-24 13:02:02 +1000

    added release optimizations

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,4 +4,4 @@ if [[ -z "$EMSDK" ]]; then
     exit 1
 fi
 [ -e build ] && rm -rf build
-cmake -B build -S . -DPLATFORM=Web -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -G Ninja
+cmake -B build -S . -DPLATFORM=Web -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -G "Ninja Multi-Config"
